a105a101262deb7f888354a35a3e140e4af3db0a
Ratndeep <ratndeepk07@gmail.com>
2020-11-29 13:36:16 +0530

    44/450 nCr

--- 450/Ratndeep/dynamic programming/nCr.py
+++ 450/Ratndeep/dynamic programming/nCr.py
@@ -1,19 +1,14 @@
+
+def find_combinations(n,r):
+    if r>n:
+        return 0
+    combinations = [0 for i in range(n+1)]
+    combinations[0]=1 
+    for i in range(1,n+1):
+        combinations[i] = i*combinations[i-1]
+                
+    return (combinations[n]//(combinations[r]*combinations[n-r]))%1000000007
+
+
 n,r = map(int,input().split())
-combinations = [[0 for i in range(n+1)] for i in range(r+1)] 
-combinations[0][0]=1 
-for i in range(1,n+1):
-    combinations[0][i]=1 
-combinations[1][0]=1
-for i in range(1,n+1):
-    combinations[1][i]=i*combinations[1][i-1]  
-
-
-
-for i in range(2,r+1):
-    for j in range(i,n+1):
-        fact = 1 
-        combinations[i][j]=combinations[1][j]/(combinations[1][j-i]*combinations[1][i])
-
-for i in combinations:
-    print(*i)
-print(combinations[r][n])
+print(find_combinations(n,r))
