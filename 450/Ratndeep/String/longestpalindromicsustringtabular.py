c9e7909cf28b9a0dc97886783d6a4b326269e104
Ratndeep <ratndeepk07@gmail.com>
2020-11-30 13:27:46 +0530

    46/450 Longest Palindromic Substring(Tabular)

---
+++ 450/Ratndeep/String/longestpalindromicsustringtabular.py
@@ -0,0 +1,57 @@
+def check(n,st,tab):
+    maxlps=1
+    for i in range(n-2,-1,-1):
+        
+        for j in range(i+1,n):
+            print(st[i],st[j])
+            if st[i]==st[j]:
+              
+                tab[i][j]=True 
+                """
+                at i=1 j=3 st[1]==st[3] 
+                
+                    a     b       c      b
+                a|True | False| False| False |
+                b|False| True | False| True  |
+                c|False| False| True | False |
+                b|False| False| False| True  |
+
+                Now, we will check that between st[1] and s[3] things are palindrome 
+                simply by checking st[2]==st[2] 
+
+                at s[0]==s[3] 
+                will check s[1] & s[2] which are not equal thus rejected
+                """
+                start,end,flag=i+1,j-1,0
+                while start<=end:
+                    if not tab[start][end]:
+                        flag=1
+                        break 
+                    start+=1 
+                    end-=1 
+                
+                if flag==0:
+                    maxlps = max(maxlps,j-i+1)  # j-i+1 gives length of current palindrome substring example 'abcb' (i=1 j=3)  length=j-i+1=3
+          
+          
+    return maxlps
+    
+
+for _ in range(int(input())):
+    st = input() 
+    n = len(st) 
+    tab = [[False for i in range(n)] for i in range(n)] 
+    for i in range(n):
+        tab[i][i]=True 
+
+    """
+    Example: 
+    st = 'abcb'
+        a     b       c      b
+    a|True | False| False| False |
+    b|False| True | False| False |
+    c|False| False| True | False |
+    b|False| False| False| True  |
+
+    """
+    print(check(n,st,tab))
