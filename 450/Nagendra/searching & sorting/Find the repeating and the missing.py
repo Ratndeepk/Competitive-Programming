08f2719bde50d01448a8f13b231cf309d3c9fe85
Nagendra Kumar <nagendra.cse1@gmail.com>
2020-12-01 20:08:17 +0530

    O(n),O(1)  Find the repeating and the missing

--- 450/Nagendra/searching & sorting/Find the repeating and the missing.py
+++ 450/Nagendra/searching & sorting/Find the repeating and the missing.py
@@ -1,20 +1,19 @@
 #link https://practice.geeksforgeeks.org/problems/find-missing-and-repeating2512/1#
-from collections import defaultdict
 
 
 class Solution:
     def findTwoElement(self, arr, n):
         # code here
-        dic = defaultdict(int)
-        lis = []
-        for i in arr:
-            dic[i] += 1
-            if dic[i] == 2:
-                lis.append(i)
 
-        for i in range(1, n+1):
-            if dic[i] <= 0:
-                lis.append(i)
+        lis = []
+        for i in range(n):
+            if arr[abs(arr[i])-1] > 0:
+                arr[abs(arr[i])-1] = -arr[abs(arr[i])-1]
+            else:
+                lis.append(abs(arr[i]))
+        for i in range(n):
+            if arr[i] > 0:
+                lis.append(i+1)
                 break
         return lis
 
