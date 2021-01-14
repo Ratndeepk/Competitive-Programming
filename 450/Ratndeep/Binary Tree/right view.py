7e7bca4e0467bc7980f027be236e6761a1878bc2
Ratndeep <ratndeepk07@gmail.com>
2021-01-14 11:25:23 +0530

    78/450 right view of bt

---
+++ 450/Ratndeep/Binary Tree/right view.py
@@ -0,0 +1,21 @@
+def height(root):
+    if root==None:
+        return 1 
+    return max(height(root.left),height(root.right))+1
+def rightview(root,v,a,level):
+    if root==None:
+        return
+    if v[level]==False:
+        a.append(root.data) 
+        v[level]=True
+    rightview(root.right,v,a,level+1) 
+    rightview(root.left,v,a,level+1)
+
+
+    
+def rightView(root):
+    a=[]
+    x = height(root)
+    v=[False]*x
+    rightview(root,v,a,0)
+    return a
