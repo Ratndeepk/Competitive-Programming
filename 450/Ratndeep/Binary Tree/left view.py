50ae8a0d3a3dc7ab19dbe7f4c633c7d1d989582b
Ratndeep <ratndeepk07@gmail.com>
2021-01-14 10:14:56 +0530

    77/450 left view of bt

---
+++ 450/Ratndeep/Binary Tree/left view.py
@@ -0,0 +1,101 @@
+
+
+
+def height(root):
+    if root==None:
+        return 1 
+    return max(height(root.left),height(root.right))+1
+def leftview(root,v,a,level):
+    if root==None:
+        return
+    if v[level]==False:
+        a.append(root.data) 
+        v[level]=True
+    leftview(root.left,v,a,level+1) 
+    leftview(root.right,v,a,level+1)
+
+def LeftView(root):
+    a=[]
+    x = height(root)
+    v=[False]*x
+    leftview(root,v,a,0)
+    return a
+
+
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        result = LeftView(root)
+        for value in result:
+            print(value,end=" ")
+        print()
+
