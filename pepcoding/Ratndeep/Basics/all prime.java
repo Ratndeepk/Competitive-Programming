144d8eb995b6a0f571631d09ee0fb3f642d8d4d4
Ratndeep <ratndeepk07@gmail.com>
2021-01-30 11:43:10 +0530

    module1 all prime

---
+++ pepcoding/Ratndeep/Basics/all prime.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int low = sc.nextInt();
+        int high = sc.nextInt();
+
+        for (int i = low; i <= high; i++) {
+            int flag = 0;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
