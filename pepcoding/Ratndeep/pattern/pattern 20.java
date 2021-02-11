14f51828ef1a53a85db945db7a72b4a35aaa6f2e
Ratndeep <ratndeepk07@gmail.com>
2021-02-11 08:35:47 +0530

    module2 pattern 20

---
+++ pepcoding/Ratndeep/pattern/pattern 20.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j==1 || j==n)
+                    System.out.print("*\t");
+                else if(i>n/2){
+                    if(i==j || j==n-i+1){
+                        System.out.print("*\t");
+                    }
+                    else
+                        System.out.print("\t");
+                }
+                else
+                    System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+}
